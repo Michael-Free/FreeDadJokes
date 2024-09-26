46652dc4d87f364d7af9f2c514c2bd47b9cccd6a
Michael Free <michael.d.free@gmail.com>
2024-09-25 23:16:08 -0400

    adding function help

--- FreeDadJokes.psm1
+++ FreeDadJokes.psm1
@@ -1,4 +1,22 @@
 function Get-DadJoke() {
+  <#
+  .SYNOPSIS
+      Get a Random Dad Joke on the command line.
+
+  .DESCRIPTION
+      This function retrieves a random dad joke from the internet.
+
+  .EXAMPLE
+      Get-DadJoke
+
+      This example retrieves a random dad joke.
+
+  .NOTES
+      Author:         Michael Free
+
+  .OUTPUTS
+        System.String. This function outputs two lines of text to the console.
+  #>
   $jokeAPI = 'https://official-joke-api.appspot.com/random_joke'
 
   $testJokeAPI = Invoke-WebRequest -Uri $jokeAPI -Method HEAD -UseBasicParsing
