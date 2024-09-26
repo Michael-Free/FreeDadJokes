f59665e54ce15c33d5ed45d5b18be86c2f72a353
Michael Free <michael.d.free@gmail.com>
2024-09-25 23:02:38 -0400

    Formatted, psd created.

--- FreeDadJokes.psm1
+++ FreeDadJokes.psm1
@@ -1,15 +1,15 @@
 function Get-DadJoke() {
-    $jokeAPI = "https://official-joke-api.appspot.com/random_joke"
+  $jokeAPI = 'https://official-joke-api.appspot.com/random_joke'
 
-    $testJokeAPI = Invoke-WebRequest -Uri $jokeAPI -Method HEAD -UseBasicParsing
-    if (-not $testJokeAPI.StatusCode -eq 200) {
-        throw 'Joke API Route Unavailable'
-    }
+  $testJokeAPI = Invoke-WebRequest -Uri $jokeAPI -Method HEAD -UseBasicParsing
+  if (-not $testJokeAPI.StatusCode -eq 200) {
+    throw 'Joke API Route Unavailable'
+  }
 
-    $dadJoke = Invoke-RestMethod -Uri $jokeAPI -Method Get
-    if (-not ($dadJoke | Get-Member -Name 'setup') -and -not ($dadJoke | Get-Member -Name 'punchline')) {
-        throw 'setup and punchline object unavailabe in response'
-    }
-    Write-Output $dadJoke.setup
-    Write-Output $dadJoke.punchline
+  $dadJoke = Invoke-RestMethod -Uri $jokeAPI -Method Get
+  if (-not ($dadJoke | Get-Member -Name 'setup') -and -not ($dadJoke | Get-Member -Name 'punchline')) {
+    throw 'setup and punchline object unavailabe in response'
+  }
+  Write-Output $dadJoke.setup
+  Write-Output $dadJoke.punchline
 }
