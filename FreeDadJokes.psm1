6ef2c1adc02c7e33a6eb6a7394af11c07d6d287a
Michael-Free <michael.d.free@gmail.com>
2025-03-25 09:25:35 -0400

    Formatting comment-based help with my typical tabs/spacing

--- FreeDadJokes.psm1
+++ FreeDadJokes.psm1
@@ -1,38 +1,38 @@
 function Get-DadJoke {
-<#
-.SYNOPSIS
-  Get stupid Dad Jokes on the Powershell commandline!
-
-.DESCRIPTION
-  Unleash the ultimate arsenal of eye-rolling, groan-inducing stupidity right from your command line! This 
-  PowerShell command fetches classic dad jokes guaranteed to make coworkers sigh, friends question your 
-  humour, and children regret asking.
-
-  Perfect for when you need a laugh—or at least to inflict one on others
-
-.PARAMETER None
-  No parameters are required for this command.
-
-.INPUTS
-  None. You cannot pipe objects to Get-DadJoke.
-
-.OUTPUTS
-  The function outputs the setup and punchline of a Dad joke in two lines of text.
-
-.EXAMPLE
-  Get-DadJoke
-
-  This command will return two lines of output. The leadup, and the joke.
-
-.NOTES
-    Author      : Michael Free
-    Date        : 2025-03-22
-    License     : Free Custom License (FCL) v1.0
-    Copyright   : 2025, Michael Free. All Rights Reserved.
-
-.LINK
-  https://github.com/Michael-Free/FreeDadJokes
-#>
+  <#
+    .SYNOPSIS
+      Get stupid Dad Jokes on the Powershell commandline!
+    
+    .DESCRIPTION
+      Unleash the ultimate arsenal of eye-rolling, groan-inducing stupidity right from your command line! This 
+      PowerShell command fetches classic dad jokes guaranteed to make coworkers sigh, friends question your 
+      humour, and children regret asking.
+    
+      Perfect for when you need a laugh—or at least to inflict one on others
+    
+    .PARAMETER None
+      No parameters are required for this command.
+    
+    .INPUTS
+      None. You cannot pipe objects to Get-DadJoke.
+    
+    .OUTPUTS
+      The function outputs the setup and punchline of a Dad joke in two lines of text.
+    
+    .EXAMPLE
+      Get-DadJoke
+    
+      This command will return two lines of output. The leadup, and the joke.
+    
+    .NOTES
+        Author      : Michael Free
+        Date        : 2025-03-22
+        License     : Free Custom License (FCL) v1.0
+        Copyright   : 2025, Michael Free. All Rights Reserved.
+    
+    .LINK
+      https://github.com/Michael-Free/FreeDadJokes
+  #>
 
   $jokeAPI = 'https://official-joke-api.appspot.com/random_joke'
 
